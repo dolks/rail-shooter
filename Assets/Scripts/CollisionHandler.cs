a53cf1a0e3af469e18824dc0283471315240030b
dolks <dylanoelkers165@gmail.com>
2022-01-06 00:02:43 -0500

    Added explosion VFX when ship crashes

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -6,15 +6,17 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
 
+    [SerializeField] ParticleSystem explosionVFX;
+
     private void OnCollisionEnter(Collision collision)
     {
         StartCoroutine(Die());
     }
 
-
     IEnumerator Die()
     {
-        GetComponent<PlayerController>().enabled = false;
+        //GetComponent<PlayerController>().enabled = false;
+        explosionVFX.Play();
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }

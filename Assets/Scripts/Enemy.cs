8c822bd83a2683973e1fd0c6c37651df648ce36a
dolks <dylanoelkers165@gmail.com>
2022-01-10 23:19:39 -0500

    Added explosion death vfx for enemies. Also added base for a score system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField] ParticleSystem deathVFX;
     private void OnParticleCollision(GameObject other)
     {
+        ParticleSystem vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        FindObjectOfType<ScoreBoard>().IncreaseScore(30);
         Destroy(gameObject);
+        Destroy(vfx, 3);
     }
 }

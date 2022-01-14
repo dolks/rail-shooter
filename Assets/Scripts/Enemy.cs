4652c12f237ee227e944b6d7d4c769f4787b91ad
dolks <dylanoelkers165@gmail.com>
2022-01-13 23:08:20 -0500

    Added enemy hit animation and controller. Added health to enemy ships. Added test scene with plane for looking at different prefabs. Added in dynamic scoring system for each defeated enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    int health = 2;
     [SerializeField] ParticleSystem deathVFX;
+    Animator enemyAnimator;
+
+    private void Start()
+    {
+        enemyAnimator = GetComponentInParent<Animator>();
+    }
+
     private void OnParticleCollision(GameObject other)
+    {
+        Hit();
+    }
+
+    void Hit()
+    {
+        enemyAnimator.Play("Enemy Hit");
+        health--;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
     {
         ParticleSystem vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         FindObjectOfType<ScoreBoard>().IncreaseScore(30);
         Destroy(gameObject);
-        Destroy(vfx, 3);
+        Destroy(vfx.gameObject, 3);
     }
+
 }
--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
 
     int score = 0;
+    TMP_Text scoreText = null;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        if (scoreText)
+        {
+            scoreText.text = score.ToString();
+        }
+    }
+
 
     public void IncreaseScore(int amount)
     {
         score += amount;
+        scoreText.text = score.ToString();
     }
 }
